077df741617a909a1a7a310048bda9032c7c36d7
Piekarz20 <102181834+Piekarz20@users.noreply.github.com>
2024-03-11 08:49:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,46 +1,14 @@
+import usersArr from './users.js';
 const lista = document.querySelector('#lista');
 const addBtn = document.querySelector('#add');
 
-let usersArr = [
-    {
-        userID: '',
-        userName: 'Bartek',
-        userCity: "Zabrze",
-        userAge: 25
-    },
-    {
-        userID: '',
-        userName: 'Adrian',
-        userCity: "Gliwice",
-        userAge: 26
-    },
-    {
-        userID: '',
-        userName: "Marek",
-        userCity: "Zielona Góra",
-        userAge: 45
-    },
-    {
-        userID: '',
-        userName: "Ania",
-        userCity: "Żywiec",
-        userAge: 36
-    },
-    {
-        userID: '',
-        userName: "Basia",
-        userCity: "Toruń",
-        userAge: 72
-    },
-]
 
 usersArr.forEach((user) =>{
-    let li = document.createElement('li')
-        li.classList.add('list-group-item');
-
-        li.textContent = `${user.userName} lat ${user.userAge} z miasta ${user.userCity}`;
-        lista.append(li);
-})
+    let li = document.createElement('li');
+    li.classList.add('list-group-item');
+    li.textContent = `${user.userName} lat ${user.userAge} z miasta ${user.userCity}`;
+    lista.append(li);
+});
 
 addBtn.addEventListener('click', (evt) => {
     evt.preventDefault();
@@ -53,7 +21,19 @@ addBtn.addEventListener('click', (evt) => {
         userName: name,
         userCity: city,
         userAge: age
-    }
-    
+    };
+
     usersArr.push(user);
-})
+    updateUsersList();
+});
+
+
+function updateUsersList() {
+    lista.innerHTML = '';
+    usersArr.forEach((user) =>{
+        let li = document.createElement('li');
+        li.classList.add('list-group-item');
+        li.textContent = `${user.userName} lat ${user.userAge} z miasta ${user.userCity}`;
+        lista.append(li);
+    });
+}
