aa10f69cf54e8f8e508f0517a6fee9f7b513c8d2
Piekarz20 <102181834+Piekarz20@users.noreply.github.com>
2024-03-11 09:11:32 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-import usersArr from './users.js';
 const lista = document.querySelector('#lista');
 const addBtn = document.querySelector('#add');
 
@@ -33,7 +32,19 @@ function updateUsersList() {
     usersArr.forEach((user) =>{
         let li = document.createElement('li');
         li.classList.add('list-group-item');
-        li.textContent = `${user.userName} lat ${user.userAge} z miasta ${user.userCity}`;
+
+        let span = document.createElement('span')
+        span.innerHTML = `<strong>${user.userName}</strong> lat <strong>${user.userAge}</strong> z miasta <strong>${user.userCity}</strong>`;
+
+        let button = document.createElement('button');
+
+        button.classList.add('btn');
+        button.classList.add('btn-outline-danger');
+        button.classList.add('delete-button')
+        button.textContent = 'X'
+
+        li.append(span);
+        li.append(button);
         lista.append(li);
     });
 }
---
+++ users.js
@@ -0,0 +1,33 @@
+let usersArr = [
+    {
+        userID: '',
+        userName: 'Bartek',
+        userCity: "Zabrze",
+        userAge: 25
+    },
+    {
+        userID: '',
+        userName: 'Adrian',
+        userCity: "Gliwice",
+        userAge: 26
+    },
+    {
+        userID: '',
+        userName: "Marek",
+        userCity: "Zielona Góra",
+        userAge: 45
+    },
+    {
+        userID: '',
+        userName: "Ania",
+        userCity: "Żywiec",
+        userAge: 36
+    },
+    {
+        userID: '',
+        userName: "Basia",
+        userCity: "Toruń",
+        userAge: 72
+    },
+]
+
