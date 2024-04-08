1228a850ea59e10734873cbf8cff1f1cff8575b3
Piekarz20 <102181834+Piekarz20@users.noreply.github.com>
2024-04-08 08:22:14 +0200

    Naprawa usuwania

--- main.js
+++ main.js
@@ -5,38 +5,45 @@ const criteria = document.querySelector('#criteria');
 const filterList = document.querySelector('#filterList');
 
 updateUsersList();
-
 addBtn.addEventListener('click', (evt) => {
-	evt.preventDefault();
-	const name = document.querySelector('#name').value;
-	const city = document.querySelector('#city').value;
-	const age = document.querySelector('#age').value;
-
-	let user = {
-		userID: crypto.randomUUID(),
-		userName: name,
-		userCity: city,
-		userAge: age,
-	};
-
-	usersArr.unshift(user);
-	updateUsersList();
+    evt.preventDefault();
+    const name = document.querySelector('#name').value;
+    const city = document.querySelector('#city').value;
+    const age = document.querySelector('#age').value;
+    let user = {
+        userID: crypto.randomUUID(),
+        userName: name,
+        userCity: city,
+        userAge: age
+    };
+    usersArr.unshift(user);
+    updateUsersList();
 });
-
 function updateUsersList(arr = usersArr) {
-	lista.innerHTML = '';
-	arr.forEach((user) => {
-		let li = document.createElement('li');
-		li.classList.add('list-group-item');
-		li.innerHTML = `<strong>${user.userName}</strong> lat <strong>${user.userAge}</strong> z miasta <strong>${user.userCity}</strong>`;
-		lista.append(li);
-	});
+    lista.innerHTML = '';
+    arr.forEach((user) =>{
+        let li = document.createElement('li');
+        li.classList.add('list-group-item');
+        let span = document.createElement('span')
+        span.innerHTML = `<strong>${user.userName}</strong> lat <strong>${user.userAge}</strong> z miasta <strong>${user.userCity}</strong>`;
+        let button = document.createElement('button');
+        button.classList.add('btn');
+        button.classList.add('btn-outline-danger');
+        button.textContent = 'X'
+        button.addEventListener('click', () =>{
+            removeUser(user.userID);
+        })
+        li.style.display = `flex`
+        li.style.justifyContent = `space-between`
+        li.append(span);
+        li.append(button);
+        lista.append(li);
+    });
 }
-
-function removeUser(removeByID) {
-	usersArr = usersArr.filter((user) => user.userID !== removeByID);
-	updateUsersList();
-}
+function removeUser(removeByID){
+    usersArr = usersArr.filter((user) => user.userID !== removeByID);
+    updateUsersList();
+};
 
 function criteriaSelect(usersArr) {
 	criteria.innerHTML = '';
